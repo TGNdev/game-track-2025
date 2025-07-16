8863b780304377d2b096d079fa3cbdd784dd2bc7
Thomas Grossmann <thomas.grossmann435gmail.com>
2025-07-16 15:17:08 +0200

    Update Header to hide controls based on route

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -16,13 +16,19 @@ const Header = () => {
     setIsModalOpen
   } = useGame();
   const allowedSearchRoutes = ["/", "/leaks-rumours", "/hall-of-fame", "/game-track-2025"];
+  const allowedContolsRoutes = ["/", "/game-track-2025"];
   const currentPath = window.location.hash
     ? window.location.hash.replace(/^#/, "")
     : window.location.pathname;
   let canSearch = true;
+  let viewControls = false;
 
   if (!allowedSearchRoutes.includes(currentPath)) {
     canSearch = false;
+  }
+
+  if (allowedContolsRoutes.includes(currentPath)) {
+    viewControls = true;
   }
 
   return (
@@ -42,70 +48,72 @@ const Header = () => {
         </div>
       </div>
 
-      <div className="flex flex-row justify-between w-full">
-        <div>
-          <button
-            type="button"
-            className={`${opened ? "animate-pulse bg-gradient-tertiary" : "bg-gradient-primary"} text-sm hover:scale-110 transition text-white px-2 py-1 rounded-md sm:hidden`}
-            onClick={() => {
-              setOpened(prev => !prev);
-              setFeaturedOpen(null);
-            }}
-          >
-            {opened ? "Collaspe all" : "Expand all"}
-          </button>
-        </div>
-        {isLogged ? (
-          <div className="flex flex-row items-center gap-2">
-            {!edit && (
-              <button
-                ref={openButtonRef}
-                className="size-6 p-1 sm:text-sm sm:w-fit sm:py-2 sm:px-2.5 sm:flex flex-row items-center bg-gradient-secondary text-white rounded-md hover:scale-110 transition"
-                onClick={() => setIsModalOpen(true)}
-              >
-                <FaPlus className="block sm:hidden" />
-                <div className="hidden sm:block">Add new game</div>
-              </button>
-            )}
+      {viewControls && (
+        <div className="flex flex-row justify-between w-full">
+          <div>
             <button
-              className={`${edit && "animate-pulse"} size-6 p-1 sm:text-sm sm:w-fit sm:py-2 sm:px-2.5 sm:flex flex-row items-center bg-gradient-tertiary text-white rounded-md hover:scale-110 transition`}
-              onClick={(e) => {
-                e.stopPropagation();
-                setEdit(prev => !prev)
-                if (!opened && !edit) {
-                  setOpened(true);
-                }
+              type="button"
+              className={`${opened ? "animate-pulse bg-gradient-tertiary" : "bg-gradient-primary"} text-sm hover:scale-110 transition text-white px-2 py-1 rounded-md sm:hidden`}
+              onClick={() => {
+                setOpened(prev => !prev);
+                setFeaturedOpen(null);
               }}
             >
-              {edit ? (
-                <FaPlus className="rotate-45 block sm:hidden" />
-              ) : (
-                <AiFillEdit className="block sm:hidden" />
-              )}
-              <div className="hidden sm:block">
-                {edit ? "Quit Edit Mode" : "Edit games"}
-              </div>
+              {opened ? "Collaspe all" : "Expand all"}
             </button>
-            {isLogged && (
-              <button
-                onClick={logout}
-                className="size-6 p-1 sm:text-sm sm:w-fit sm:py-2 sm:px-2.5 sm:flex flex-row items-center bg-gradient-primary text-white rounded-md hover:scale-110 transition"
-              >
-                <FaSignOutAlt className="block sm:hidden" />
-                <div className="hidden sm:block">Logout</div>
-              </button>
-            )}
           </div>
-        ) : (
-          <button
-            ref={openButtonRef}
-            className="text-sm sm:w-fit sm:py-2 px-2.5 sm:flex flex-row items-center bg-gradient-primary text-white rounded-md hover:scale-110 transition"
-            onClick={() => setIsModalOpen(true)}
-          >
-            <div className="">I am an admin</div>
-          </button>
-        )}
-      </div>
+          {isLogged ? (
+            <div className="flex flex-row items-center gap-2">
+              {!edit && (
+                <button
+                  ref={openButtonRef}
+                  className="size-6 p-1 sm:text-sm sm:w-fit sm:py-2 sm:px-2.5 sm:flex flex-row items-center bg-gradient-secondary text-white rounded-md hover:scale-110 transition"
+                  onClick={() => setIsModalOpen(true)}
+                >
+                  <FaPlus className="block sm:hidden" />
+                  <div className="hidden sm:block">Add new game</div>
+                </button>
+              )}
+              <button
+                className={`${edit && "animate-pulse"} size-6 p-1 sm:text-sm sm:w-fit sm:py-2 sm:px-2.5 sm:flex flex-row items-center bg-gradient-tertiary text-white rounded-md hover:scale-110 transition`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setEdit(prev => !prev)
+                  if (!opened && !edit) {
+                    setOpened(true);
+                  }
+                }}
+              >
+                {edit ? (
+                  <FaPlus className="rotate-45 block sm:hidden" />
+                ) : (
+                  <AiFillEdit className="block sm:hidden" />
+                )}
+                <div className="hidden sm:block">
+                  {edit ? "Quit Edit Mode" : "Edit games"}
+                </div>
+              </button>
+              {isLogged && (
+                <button
+                  onClick={logout}
+                  className="size-6 p-1 sm:text-sm sm:w-fit sm:py-2 sm:px-2.5 sm:flex flex-row items-center bg-gradient-primary text-white rounded-md hover:scale-110 transition"
+                >
+                  <FaSignOutAlt className="block sm:hidden" />
+                  <div className="hidden sm:block">Logout</div>
+                </button>
+              )}
+            </div>
+          ) : (
+            <button
+              ref={openButtonRef}
+              className="text-sm sm:w-fit sm:py-2 px-2.5 sm:flex flex-row items-center bg-gradient-primary text-white rounded-md hover:scale-110 transition"
+              onClick={() => setIsModalOpen(true)}
+            >
+              <div className="">I am an admin</div>
+            </button>
+          )}
+        </div>
+      )}
     </div>
   );
 }
