f8f68d2e4108e33211b81462901a5ac1f41e9ac9
Thomas Grossmann <thomas.grossmann435gmail.com>
2026-01-13 11:33:31 +0100

    Update ReleaseCalendar view page to have an unified calendar with only data switch between games and events

--- src/pages/ReleaseCalendar.jsx
+++ src/pages/ReleaseCalendar.jsx
@@ -1,6 +1,5 @@
 import Layout from "../components/shared/Layout";
-import Events from "../components/calendar/Events";
-import Games from "../components/calendar/Games";
+import Calendar from "../components/calendar/Calendar";
 import { useState } from "react";
 
 const ReleaseCalendar = () => {
@@ -16,32 +15,26 @@ const ReleaseCalendar = () => {
           <div className="border-primary rounded-xl">
             <div className="flex flex-row gap-4 items-center justify-center p-2 rounded-md">
               <button
-              className={`w-fit px-2 py-1.5 sm:px-3 sm:py-2 rounded-md text-sm sm:text-base
+                className={`w-fit px-2 py-1.5 sm:px-3 sm:py-2 rounded-md text-sm sm:text-base
                 ${seeGames ? 'bg-gradient-primary' : ''}`}
-              onClick={() => setView('games')}
-              disabled={seeGames}
-            >
-              Games
-            </button>
-            <button
-              className={`w-fit px-2 py-1.5 sm:px-3 sm:py-2 rounded-md text-sm sm:text-base
+                onClick={() => setView('games')}
+                disabled={seeGames}
+              >
+                Games
+              </button>
+              <button
+                className={`w-fit px-2 py-1.5 sm:px-3 sm:py-2 rounded-md text-sm sm:text-base
                 ${seeEvents ? 'bg-gradient-primary' : ''}`}
-              onClick={() => setView('events')}
-              disabled={seeEvents}
-            >
-              Events
-            </button>
+                onClick={() => setView('events')}
+                disabled={seeEvents}
+              >
+                Events
+              </button>
             </div>
           </div>
         </div>
         <div className="w-full flex flex-col justify-between">
-          {seeGames ? (
-            <Games />
-          ) : seeEvents ? (
-            <Events />
-          ) : (
-            <div></div>
-          )}
+          <Calendar mode={view} />
         </div>
       </div>
     </Layout>
