8cc285111c02b740cd69bcf3f70b7fa618ab8b67
Thomas Grossmann <thomas.grossmann435gmail.com>
2026-01-15 09:36:07 +0100

    Register AuthContext in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { HashRouter } from "react-router-dom";
 import { GameProvider } from "./contexts/GameProvider";
+import { AuthProvider } from "./contexts/AuthContext";
 import AnimatedRoutes from "./routes/AnimatedRoutes";
 
 const App = () => {
   return (
-    <GameProvider>
-      <HashRouter>
-        <div className="min-h-screen flex flex-col">
-          <AnimatedRoutes />
-        </div>
-      </HashRouter>
-    </GameProvider>
+    <AuthProvider>
+      <GameProvider>
+        <HashRouter>
+          <div className="min-h-screen flex flex-col">
+            <AnimatedRoutes />
+          </div>
+        </HashRouter>
+      </GameProvider>
+    </AuthProvider>
   );
 };
 
