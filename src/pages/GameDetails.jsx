337f5bbef5c2e2505eaec3df906d7b23f49b560d
Thomas Grossmann <thomas.grossmann435gmail.com>
2026-01-15 09:41:04 +0100

    Update GameDetails with user related features like add to library and create countdown for a game

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState, useMemo } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
-import { FaArrowLeft, FaClock, FaTrophy, FaGamepad, FaBuilding, FaCalendarAlt, FaExternalLinkAlt } from "react-icons/fa";
+import { FaArrowLeft, FaClock, FaTrophy, FaCalendarAlt, FaExternalLinkAlt, FaPlus, FaCheck, FaImage } from "react-icons/fa";
 import Layout from "../components/shared/Layout";
 import { useGameData } from "../contexts/GameDataContext";
 import { useGameUI } from "../contexts/GameUIContext";
+import { useAuth } from "../contexts/AuthContext";
 import { slugify } from "../js/utils";
 import { getGameCovers, getGameScreenshots, getGameTimeToBeat } from "../js/igdb";
+import { addToLibrary, removeFromLibrary, addCountdown, removeCountdown } from "../js/firebase";
+import { toast } from "react-toastify";
 import CoverSkeleton from "../components/skeletons/CoverSkeleton";
 import he from "he";
+import { FiClock } from "react-icons/fi";
 
 const getRatingStyle = (rating) => {
   const baseClasses = "size-12 rounded-xl text-white font-bold flex items-center justify-center text-lg shadow-lg";
@@ -30,6 +34,7 @@ export default function GameDetails() {
   const [coverLoaded, setCoverLoaded] = useState(false);
   const [currentScreenshotIndex, setCurrentScreenshotIndex] = useState(0);
   const [showAllScreenshots, setShowAllScreenshots] = useState(false);
+  const { userData, currentUser } = useAuth();
 
   const {
     games,
@@ -49,13 +54,53 @@ export default function GameDetails() {
     return games.find(g => slugify(g.name) === gameSlug);
   }, [games, gameSlug]);
 
+  const isInLibrary = useMemo(() => userData?.library?.includes(game?.id), [userData?.library, game?.id]);
+  const hasCountdown = useMemo(() => userData?.wanted?.includes(game?.id), [userData?.wanted, game?.id]);
+
+  const handleLibraryAction = async () => {
+    if (!currentUser) {
+      toast.info("Please log in to add games to your library");
+      return;
+    }
+    try {
+      if (isInLibrary) {
+        await removeFromLibrary(currentUser.uid, game.id);
+        toast.info(`${game.name} removed from library`);
+      } else {
+        await addToLibrary(currentUser.uid, game.id);
+        toast.success(`${game.name} added to library !`);
+      }
+    } catch (e) {
+      toast.error("An error occurred. Please try again.");
+    }
+  };
+
+  const handleCountdownAction = async () => {
+    if (!currentUser) {
+      toast.info("Please log in to add countdown for this game");
+      return;
+    }
+    try {
+      if (hasCountdown) {
+        await removeCountdown(currentUser.uid, game.id);
+        toast.info(`Countdown removed for ${game.name}`);
+      } else {
+        await addCountdown(currentUser.uid, game.id);
+        toast.success(`Countdown added for ${game.name}`);
+      }
+    } catch (e) {
+      toast.error("An error occurred. Please try again.");
+    }
+  };
+
   const gameAwards = awardsPerGame[game?.id] || [];
   const gameTimeToBeat = timesToBeat[game?.igdb_id];
   const gameScreenshots = useMemo(() => screenshotsMap[game?.igdb_id] || [], [screenshotsMap, game?.igdb_id]);
   const displayedScreenshots = showAllScreenshots
-    ? gameScreenshots.slice(1)
-    : gameScreenshots.slice(1, 5);
+    ? gameScreenshots.slice(0)
+    : gameScreenshots.slice(0, 4);
   const gameCover = coverMap[game?.igdb_id];
+  const canAddCountdown = game?.release_date?.seconds;
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -129,7 +174,7 @@ export default function GameDetails() {
   return (
     <Layout>
       <div className="w-full flex flex-col gap-8 pb-12">
-        <div className="relative w-full h-[45vh] min-h-[350px] overflow-hidden shadow-2xl">
+        <div className="relative w-full h-[40vh] min-h-[350px] overflow-hidden shadow-2xl opacity-70">
           {gameScreenshots.length > 0 ? (
             gameScreenshots.slice(0, 6).map((shot, idx) => (
               <img
@@ -165,10 +210,36 @@ export default function GameDetails() {
                 />
               )}
             </div>
+            {currentUser ? (
+              <div className="absolute -bottom-4 left-1/2 -translate-x-1/2 flex gap-2">
+                <button
+                  onClick={handleLibraryAction}
+                  className={`p-3 rounded-full shadow-xl transition-all duration-300 hover:scale-105 ${isInLibrary ? 'bg-gradient-secondary' : 'bg-gradient-primary'}`}
+                >
+                  {isInLibrary ? <FaCheck /> : <FaPlus />}
+                </button>
+                {canAddCountdown && (
+                  <button
+                    onClick={handleCountdownAction}
+                    className={`p-3 rounded-full shadow-xl transition-all duration-300 hover:scale-105 ${hasCountdown ? 'bg-gradient-tertiary' : 'bg-gradient-primary'}`}
+                  >
+                    <FiClock />
+                  </button>
+                )}
+              </div>
+            ) : (
+              <div className="absolute -bottom-3.5 left-1/2 -translate-x-1/2 flex gap-2 w-full justify-center">
+                <div
+                  className="flex items-center gap-2 text-sm bg-white/30 backdrop-blur-md px-2 py-1.5 rounded-md w-fit font-semibold"
+                >
+                  Login to discover actions
+                </div>
+              </div>
+            )}
           </div>
-          <div className="flex-1 space-y-6 pt-16 md:pt-32">
+          <div className="flex-1 space-y-6 pt-6 md:pt-32 w-full">
             <div>
-              <h1 className="text-4xl md:text-6xl font-black mb-4 tracking-tight leading-tight">
+              <h1 className="text-4xl md:text-6xl font-black mb-4 w-full md:text-left text-center">
                 {he.decode(game.name)}
               </h1>
               <div className="flex flex-wrap gap-6 items-center text-white/80">
@@ -185,7 +256,7 @@ export default function GameDetails() {
                   </span>
                 </div>
                 <div className="flex items-center gap-3">
-                  <span className="text-xs font-bold uppercase tracking-widest text-white/70">Available on</span>
+                  <span className="text-xs font-bold uppercase text-white/70">Available on</span>
                   <div className="flex gap-2">
                     {Object.keys(game.platforms)
                       .filter((p) => game.platforms[p])
@@ -198,41 +269,45 @@ export default function GameDetails() {
                 </div>
               </div>
             </div>
-            <div className="flex flex-wrap gap-6">
-              <div className="flex flex-col items-center gap-2">
-                <div className={getRatingStyle(Number(game.ratings.critics))}>
-                  {Number(game.ratings.critics) === 0 ? "/" : game.ratings.critics}
+            <div className="flex flex-wrap gap-6 items-center">
+              <div className="flex gap-6">
+                <div className="flex flex-col items-center gap-2">
+                  <div className={getRatingStyle(Number(game.ratings.critics))}>
+                    {Number(game.ratings.critics) === 0 ? "/" : game.ratings.critics}
+                  </div>
+                  <span className="text-[10px] font-bold uppercase text-white/50 text-center">Critics<br />Rating</span>
                 </div>
-                <span className="text-[10px] font-bold uppercase tracking-widest text-white/50 text-center">Critics<br />Score</span>
-              </div>
-              <div className="flex flex-col items-center gap-2">
-                <div className={getRatingStyle(Number(game.ratings.players))}>
-                  {Number(game.ratings.players) === 0 ? "/" : game.ratings.players}
+                <div className="flex flex-col items-center gap-2">
+                  <div className={getRatingStyle(Number(game.ratings.players))}>
+                    {Number(game.ratings.players) === 0 ? "/" : game.ratings.players}
+                  </div>
+                  <span className="text-[10px] font-bold uppercase text-white/50 text-center">Players<br />Rating</span>
                 </div>
-                <span className="text-[10px] font-bold uppercase tracking-widest text-white/50 text-center">Players<br />Score</span>
               </div>
-              {game.ratings.link && (
-                <a
-                  href={game.ratings.link}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="flex items-center gap-2 text-sm bg-gradient-primary px-6 py-3 rounded-2xl h-fit transition hover:scale-105 self-center md:self-start font-bold shadow-lg"
-                >
-                  OpenCritic Details <FaExternalLinkAlt className="text-[10px]" />
-                </a>
-              )}
+              <div className="flex flex-wrap gap-3">
+                {game.ratings.link && (
+                  <a
+                    href={game.ratings.link}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="flex items-center gap-2 text-sm bg-gradient-primary px-2 py-1.5 rounded-md h-fit transition hover:scale-105 self-center font-bold shadow-lg"
+                  >
+                    OpenCritic <FaExternalLinkAlt className="text-[10px]" />
+                  </a>
+                )}
+              </div>
             </div>
           </div>
         </div>
         <div className="px-6 grid grid-cols-1 lg:grid-cols-3 gap-8">
-          <div className="lg:col-span-2 space-y-12">
+          <div className="lg:col-span-2 space-y-12 mt-6">
             {gameAwards.length > 0 && (
-              <section className="bg-black/20 backdrop-blur-sm border border-white/10 rounded-[2rem] p-6 md:p-8">
+              <section className="bg-white/5 shadow-2xl border border-white/10 rounded-2xl p-6">
                 <div className="flex items-center gap-4 mb-6">
-                  <div className="p-3 bg-gradient-tertiary rounded-xl shadow-xl">
+                  <div className="p-3 bg-gradient-tertiary rounded-lg">
                     <FaTrophy className="text-2xl text-white" />
                   </div>
-                  <h2 className="text-2xl font-black tracking-tight">Awards Won</h2>
+                  <h2 className="text-2xl font-black">Awards Won</h2>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   {gameAwards.map((award, i) => (
@@ -256,16 +331,16 @@ export default function GameDetails() {
             {gameScreenshots.length > 1 && (
               <section className="space-y-6">
                 <div className="flex items-center justify-between">
-                  <h2 className="text-3xl font-black tracking-tight flex items-center gap-4">
-                    <div className="p-3 bg-gradient-primary rounded-2xl shrink-0 shadow-lg"><FaGamepad className="text-xl" /></div>
-                    Screenshots
+                  <h2 className="text-3xl font-black flex items-center gap-4">
+                    <div className="p-3 bg-gradient-primary rounded-lg shrink-0 shadow-lg"><FaImage className="text-xl" /></div>
+                    Screenshots ({gameScreenshots.length})
                   </h2>
                   {gameScreenshots.length > 5 && (
                     <button
                       onClick={() => setShowAllScreenshots(!showAllScreenshots)}
-                      className="text-sm font-bold bg-gradient-primary px-4 py-2 rounded-xl transition hover:scale-105 shadow-md"
+                      className="text-sm font-semibold bg-gradient-primary px-2 py-1.5 rounded-md transition hover:scale-105 shadow-md"
                     >
-                      {showAllScreenshots ? "See Less" : `See All (${gameScreenshots.length})`}
+                      {showAllScreenshots ? "See Less" : "See All"}
                     </button>
                   )}
                 </div>
@@ -284,17 +359,22 @@ export default function GameDetails() {
               </section>
             )}
           </div>
-          <div className="space-y-8">
+          <div className="space-y-8 mt-6">
             {gameTimeToBeat && (
-              <section className="bg-gradient-to-br from-[#1a1c2e] to-[#0f111a] border border-white/10 rounded-[2rem] p-8 shadow-2xl relative overflow-hidden group">
-                <div className="absolute top-0 right-0 p-6 opacity-10 group-hover:scale-110 transition duration-700 ease-out pointer-events-none">
+              <section className="bg-white/5 border border-white/10 rounded-2xl p-6 shadow-2xl relative overflow-hidden">
+                <div className="absolute top-0 right-0 p-6 opacity-10 pointer-events-none">
                   <FaClock className="text-8xl" />
                 </div>
                 <div className="relative z-10">
-                  <h2 className="text-xl font-black mb-6 flex items-center gap-3">
-                    <FaClock className="text-primary-light" /> Playtime
+                  <h2 className="text-xl font-black mb-6">
+                    Playtime
                   </h2>
                   <div className="space-y-6">
+                    <div className="flex justify-between items-end">
+                      <p className="text-white/60 text-[10px] font-black uppercase tracking-widest -mt-4 mb-3 border border-white/20 p-2 rounded-lg backdrop-blur-sm">
+                        Please note that this data is contributed by the community (via <span className="text-white">IGDB</span>) and may not exactly reflect your personal playthroughs.
+                      </p>
+                    </div>
                     <div className="flex justify-between items-end border-b border-white/10 pb-4">
                       <div className="flex flex-col">
                         <span className="text-white/40 text-[10px] font-black uppercase tracking-widest mb-1">Main Story</span>
@@ -309,7 +389,7 @@ export default function GameDetails() {
                       </div>
                       <span className="text-2xl font-black text-primary-light">{formatTime(gameTimeToBeat.hastily)}</span>
                     </div>
-                    <div className="flex justify-between items-end">
+                    <div className="flex justify-between items-end ">
                       <div className="flex flex-col">
                         <span className="text-white/40 text-[10px] font-black uppercase tracking-widest mb-1">Completionist</span>
                         <span className="text-white font-bold">100% Run</span>
@@ -320,10 +400,10 @@ export default function GameDetails() {
                 </div>
               </section>
             )}
-            <section className="bg-white/5 backdrop-blur-sm border border-white/10 rounded-[2rem] p-8 space-y-6">
+            <section className="bg-white/5 border border-white/10 rounded-2xl p-6 space-y-6 shadow-2xl ">
               <div>
-                <h3 className="text-[10px] font-black uppercase tracking-widest text-primary-light mb-4 flex items-center gap-2">
-                  <FaBuilding className="text-xs" /> Developers
+                <h3 className="text-xs font-black uppercase text-primary-light mb-4 flex items-center gap-2">
+                  Developers
                 </h3>
                 <div className="flex flex-col gap-3">
                   {game.developers.map((dev, i) => (
@@ -332,17 +412,17 @@ export default function GameDetails() {
                       href={dev.link}
                       target="_blank"
                       rel="noreferrer"
-                      className="text-lg font-black hover:text-primary-light transition flex items-center justify-between group hover:scale-105"
+                      className="text-lg font-black transition flex items-center flex-row gap-2 hover:scale-105"
                     >
                       {he.decode(dev.name)}
-                      <FaExternalLinkAlt className="text-[10px] opacity-0 group-hover:opacity-40 transition" />
+                      <FaExternalLinkAlt className="text-[10px]" />
                     </a>
                   ))}
                 </div>
               </div>
               <div className="pt-6 border-t border-white/10">
-                <h3 className="text-[10px] font-black uppercase tracking-widest text-primary-light mb-4 flex items-center gap-2">
-                  <FaBuilding className="text-xs" /> Publishers
+                <h3 className="text-xs font-black uppercase text-primary-light mb-4 flex items-center gap-2">
+                  Publishers
                 </h3>
                 <div className="flex flex-col gap-3">
                   {game.editors.map((editor, i) => (
@@ -351,10 +431,10 @@ export default function GameDetails() {
                       href={editor.link}
                       target="_blank"
                       rel="noreferrer"
-                      className="text-lg font-black hover:text-primary-light transition flex items-center justify-between group hover:scale-105"
+                      className="text-lg font-black transition flex items-center flex-row gap-2 hover:scale-105"
                     >
                       {he.decode(editor.name)}
-                      <FaExternalLinkAlt className="text-[10px] opacity-0 group-hover:opacity-40 transition" />
+                      <FaExternalLinkAlt className="text-[10px]" />
                     </a>
                   ))}
                 </div>
